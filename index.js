14143da295e2cd0e49fe8f201e298eecb6dbd04c
thetejasmagade <53045273+thetejasmagade@users.noreply.github.com>
2022-12-28 19:01:03 +0530

    Added: Payment Integration

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+var cors = require('cors')
 const eventRouter = require('./routes/event')
 const userRouter = require('./routes/user')
+const paymentRouter = require('./routes/payment')
 const connectToMongo = require('./db');
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
+
+app.use(cors())
 
 connectToMongo();
 
@@ -15,6 +19,26 @@ app.use('/api/v1/u', userRouter);
 
 
 
-app.listen(4000, () => {
+
+// Razorpay Payment
+app.use("/payment", require("./routes/payment"));
+
+app.post('/orders', (req, res) => {
+  res.send({
+    "amount": 20000,
+    "currency": "INR",
+    "orderid": "rtadsqsf"
+  });
+});
+
+app.post('/success', (req, res) => {
+  console.log(req.body);
+  res.send(req.body)
+})
+
+
+
+
+app.listen(3000, () => {
   console.log('Server listening on port 3000');
 });
